45e562e0aaa2946cfd18dad2929de7fe58a57797
Hyunje Park <hjp1nku@korea.ac.kr>
2025-05-06 16:53:34 +0900

    ..

--- env_constants.py
+++ env_constants.py
@@ -6,3 +6,7 @@ if hostname == "hjpui-MacBookPro.local":
     # PROJECT_ROOT    = "/Users/hjp/HJP/KUAICV/VTO/florence-tailor"
     DATASET_ROOT    = "/Users/hjp/HJP/KUAICV/VTO/DATASET/PoC59"
     PYGARMENT_ROOT  = "/Users/hjp/HJP/KUAICV/VTO/GarmentCodeAnalysis"
+
+elif hostname == "hjp-Z690-AERO-G":
+    DATASET_ROOT = ""
+    PYGARMENT_ROOT = r"E:\HJP\KUAICV\VTO\REFERENCES\GarmentCodeAnalysis"
