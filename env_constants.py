fccaabf95d5e8ce810a1793ecdd5da6bb9fb058d
Hyunje Park <hjp1nskku@g.skku.edu>
2025-05-11 23:27:32 +0900

    ..

--- env_constants.py
+++ env_constants.py
@@ -6,7 +6,9 @@ if hostname == "hjpui-MacBookPro.local":
     # PROJECT_ROOT    = "/Users/hjp/HJP/KUAICV/VTO/florence-tailor"
     DATASET_ROOT    = "/Users/hjp/HJP/KUAICV/VTO/DATASET/PoC59"
     PYGARMENT_ROOT  = "/Users/hjp/HJP/KUAICV/VTO/GarmentCodeAnalysis"
-
+    SEWFACTORY_BASE_PATH = "/Users/hjp/HJP/KUAICV/VTO/DATASET/sewfactory/sewfactory"
+    
 elif hostname == "hjp-Z690-AERO-G":
     DATASET_ROOT = ""
     PYGARMENT_ROOT = r"E:\HJP\KUAICV\VTO\REFERENCES\GarmentCodeAnalysis"
+    SEWFACTORY_BASE_PATH = r"D:\VTO2025\DATASETs\SewFactory\sewfactory"
